6d5d77c16246fbc7b7c4c044cb1a6dc57985d78f
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2020-04-09 06:56:03 -0700

    updated the code

--- main.py
+++ main.py
@@ -3,75 +3,78 @@ import random
 
 screen_size = [360, 600]
 screen = pygame.display.set_mode(screen_size)
-score = 0
-green = (0, 255, 0)
 pygame.font.init()
 
-
-def load(name):
-    return pygame.image.load(name)
+background = pygame.image.load('background.png')
+user = pygame.image.load('user.png')
+chicken = pygame.image.load('chicken.png')
 
 
 def display_score(score):
     font = pygame.font.SysFont('Comic Sans MS', 30)
     score_text = 'Score: ' + str(score)
-    text = font.render(score_text, True, green)
-    screen.blit(text, [20, 10])
+    text_img = font.render(score_text, True, (0, 255, 0))
+    screen.blit(text_img, [20, 10])
 
 
-def get_rand_offset():
-    return 100*random.randint(5, 15)
+def random_offset():
+    return -1*random.randint(100, 1500)
 
 
-def set_s_position(idx, pos):
+chicken_y = [random_offset(), random_offset(), random_offset()]
+user_x = 150
+score = 0
+
+
+def crashed(idx):
     global score
-    if c_positions[idx] > 600:
-        c_positions[idx] = 0 - get_rand_offset()
-        score = score + 50
+    global keep_alive
+    score = score - 50
+    chicken_y[idx] = random_offset()
+    if score < -500:
+        keep_alive = False
+
+
+def update_chicken_pos(idx):
+    global score
+    if chicken_y[idx] > 600:
+        chicken_y[idx] = random_offset()
+        score = score + 5
+        print('score', score)
     else:
-        c_positions[idx] += 5
+        chicken_y[idx] = chicken_y[idx] + 5
 
-
-background = load('background.png')
-chicken = load('chicken.png')
-user = load('user.png')
-user_x = 160
-c_positions = [0-get_rand_offset(), 0-get_rand_offset(), 0]
 
 keep_alive = True
 clock = pygame.time.Clock()
-
 while keep_alive:
     pygame.event.get()
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] and user_x < 300:
+    if keys[pygame.K_RIGHT] and user_x < 280:
         user_x = user_x + 10
     elif keys[pygame.K_LEFT] and user_x > 0:
         user_x = user_x - 10
 
-    set_s_position(0, 40)
-    set_s_position(1, 160)
-    set_s_position(2, 280)
+    update_chicken_pos(0)
+    update_chicken_pos(1)
+    update_chicken_pos(2)
 
     screen.blit(background, [0, 0])
-    screen.blit(chicken, [40, c_positions[0]])
-    screen.blit(chicken, [160, c_positions[1]])
-    screen.blit(chicken, [280, c_positions[2]])
+    screen.blit(user, [user_x, 520])
+    screen.blit(chicken, [0, chicken_y[0]])
+    screen.blit(chicken, [150, chicken_y[1]])
+    screen.blit(chicken, [280, chicken_y[2]])
+
+    if chicken_y[0] > 500 and user_x < 70:
+        crashed(0)
+
+    if chicken_y[1] > 500 and user_x > 80 and user_x < 200:
+        crashed(1)
+
+    if chicken_y[2] > 500 and user_x > 220:
+        crashed(2)
 
     display_score(score)
-    screen.blit(user, [user_x, 500])
-
-    if c_positions[0] > 500 and user_x < 100:
-        print('crash 1', user_x)
-        score = score - 50
-
-    if c_positions[2] > 500 and user_x > 200:
-        print('crash 3', user_x)
-        score = score - 50
-
-    if c_positions[1] > 500 and user_x > 100 and user_x < 200:
-        print('crash 2', user_x)
-        score = score - 50
 
     pygame.display.update()
     clock.tick(60)
